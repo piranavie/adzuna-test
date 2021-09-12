e127dd37cbc80e3b3525785a2ff90e6bda575edc
piranavie <60823650+piranavie@users.noreply.github.com>
2021-09-12 22:14:42 +0100

    save

--- src/App/App.js
+++ src/App/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-        <h2>Aduze Test</h2>
+        <h2>Adzuna Test</h2>
         <Form />
         </div>
       </div>
